8b710ca18c9ac8e97a123fdb581b1f401e7a0c72
JAYARAJ SIVADASAN <jayaraj.sivadasan@gmail.com>
2019-12-22 19:41:49 -0500

    Simple validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +8,36 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent {
   title = 'reactive';
+  constructor(private fb: FormBuilder) {
+
+  }
 
   /*Create the form Model
   To associate this model with the HTML , we have bound [fromGroup] in the form 
   and formControlName in the individual controls. 
   */
-  regForm = new FormGroup({
-    userName: new FormControl('test'),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    address: new FormGroup({
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
+  // regForm = new FormGroup({
+  //   userName: new FormControl('test'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     zip: new FormControl('')
+  //   })
+  // });
+  regForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(4)]],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''], state: [''], zip: ['']
     })
-  });
+  })
 
+  get userName() {
+    return this.regForm.get('userName');
+  }
   loadData() {
     this.regForm.patchValue({
       userName: 'Jayaraj',
@@ -32,7 +46,7 @@ export class AppComponent {
       address: {
         city: 'Ekm',
         state: 'Kerala',
-        zip:'682315'
+        zip: '682315'
       }
     });
   }
