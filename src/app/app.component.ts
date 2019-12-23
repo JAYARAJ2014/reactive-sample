d0d687cd90836b43e9884ed84084c9d87d62fb60
JAYARAJ SIVADASAN <jayaraj.sivadasan@gmail.com>
2019-12-22 20:46:22 -0500

    Conditional Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validator, Validators } from '@angular/forms';
 import { ForbiddenNameValidator } from './shared/user-name.validator';
 import { PasswordValidator } from './shared/password.validator';
@@ -8,12 +8,43 @@ import { PasswordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'reactive';
+  regForm : FormGroup; 
+  get userName() {
+    return this.regForm.get('userName');
+  }
+  get email () {
+    return this.regForm.get('email');
+  }
   constructor(private fb: FormBuilder) {
 
   }
+  ngOnInit(): void {
+    
+    this.regForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(4), ForbiddenNameValidator(/admin/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''], state: [''], zip: ['']
+      })
+    }, {validator: PasswordValidator});
 
+    this.regForm.get('subscribe').valueChanges
+    .subscribe(checkedValue=> {
+      const email = this.regForm.get('email');
+      if(checkedValue ) {
+        email.setValidators(Validators.required);
+      }else {
+        email.clearValidators();
+      }
+      email.updateValueAndValidity();
+    })
+  }
   /*Create the form Model
   To associate this model with the HTML , we have bound [fromGroup] in the form 
   and formControlName in the individual controls. 
@@ -28,18 +59,9 @@ export class AppComponent {
   //     zip: new FormControl('')
   //   })
   // });
-  regForm = this.fb.group({
-    userName: ['', [Validators.required, Validators.minLength(4), ForbiddenNameValidator(/admin/)]],
-    password: [''],
-    confirmPassword: [''],
-    address: this.fb.group({
-      city: [''], state: [''], zip: ['']
-    })
-  }, {validator: PasswordValidator})
+  
 
-  get userName() {
-    return this.regForm.get('userName');
-  }
+ 
   loadData() {
     this.regForm.patchValue({
       userName: 'Jayaraj',
